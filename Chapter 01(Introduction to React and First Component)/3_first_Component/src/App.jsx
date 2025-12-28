dd0a71617973ab016715d09a19541714bfc11319
UsaaryanByte07 <usaaryan79@gmail.com>
2025-12-29 00:07:28 +0530

    Added Chapter 1: Introduction to React and First Component examples

---
+++ Chapter 01(Introduction to React and First Component)/3_first_Component/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Heading from './components/Heading';
+import Paragraph from './components/Paragraph';
+
+function App() {
+
+  return (
+    <>
+      <Heading />
+      <Paragraph />
+    </>
+  )
+}
+
+export default App;
